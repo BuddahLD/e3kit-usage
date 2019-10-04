04c7d786a0798a3be0c8b6c63e1b2e7809ec9612
BuddahLD <danylo.oliinyk@gmail.com>
2019-10-04 17:40:13 +0300

    Initial commit

---
+++ app/src/main/java/com/virgilsecurity/android/e3kitusage/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.virgilsecurity.android.e3kitusage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
